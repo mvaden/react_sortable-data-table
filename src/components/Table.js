3d3d5421cca9bc376c030ca1827a898652216428
mvaden <mvaden08@gmail.com>
2023-06-10 14:38:53 -0400

    Set up set up Table component to handle TableHead and TableBody components.

---
+++ src/components/Table.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import TableBody from "./TableBody";
+import TableHead from "./TableHead";
+
+const Table = () => {
+   const importedData = require("../data/data.json");
+   const [tableData, setTableData] = useState(importedData);
+
+   const columns = [
+      {
+         label: "First Name",
+         accessor: "first_name",
+      },
+      {
+         label: "Last Name",
+         accessor: "last_name",
+      },
+      {
+         label: "Email",
+         accessor: "email",
+      },
+      {
+         label: "Gender",
+         accessor: "gender",
+      },
+      {
+         label: "IP Address",
+         accessor: "ip_address",
+      },
+   ];
+
+   return (
+      <>
+         <table className="table">
+            <caption>Developers currently enrolled in this course, column headers are sortable.</caption>
+            <TableHead columns={columns} />
+            <TableBody columns={columns} tableData={tableData} />
+         </table>
+      </>
+   );
+};
+
+export default Table;
