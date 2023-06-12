be52d2dc9a9bba4bc8df2aabe56fcebd8aef0693
mvaden <mvaden08@gmail.com>
2023-06-12 00:26:41 -0400

    Handle Columns and table data props at the root level

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,39 +1,15 @@
-import { useState } from "react";
 import TableBody from "../TableBody/TableBody";
 import TableHead from "../TableHead/TableHead";
+import { useTableSorting } from "../hooks/useSort";
 
-const Table = () => {
-   const importedData = require("../../data/data.json");
-   const [tableData, setTableData] = useState(importedData);
-
-   const columns = [
-      {
-         label: "First Name",
-         accessor: "first_name",
-      },
-      {
-         label: "Last Name",
-         accessor: "last_name",
-      },
-      {
-         label: "Email",
-         accessor: "email",
-      },
-      {
-         label: "Gender",
-         accessor: "gender",
-      },
-      {
-         label: "IP Address",
-         accessor: "ip_address",
-      },
-   ];
+const Table = ({ caption, data, columns }) => {
+   const [tableData, handleSorting] = useTableSorting(data, columns);
 
    return (
       <>
          <table className="table">
-            <caption>Developers currently enrolled in this course, column headers are sortable.</caption>
-            <TableHead columns={columns} />
+            <caption>{caption}</caption>
+            <TableHead columns={columns} handleSorting={handleSorting} />
             <TableBody columns={columns} tableData={tableData} />
          </table>
       </>
