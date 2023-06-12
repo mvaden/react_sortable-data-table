4ae6048198afff587aa813a719c67bebeda70720
mvaden <mvaden08@gmail.com>
2023-06-12 00:24:44 -0400

    Pass caption, columns, data as props into Table

--- src/App.js
+++ src/App.js
@@ -2,10 +2,39 @@ import Table from './components/Table/Table';
 import './App.scss';
 
 const App = () => {
+  const caption = "Developers currently enrolled in this course, column headers are sortable.";
+  const data = require("./data/data.json");
+  const columns = [
+    {
+      label: "First Name",
+      accessor: "first_name",
+    },
+    {
+      label: "Last Name",
+      accessor: "last_name",
+    },
+    {
+      label: "Email",
+      accessor: "email",
+    },
+    {
+      label: "Gender",
+      accessor: "gender",
+    },
+    {
+      label: "IP Address",
+      accessor: "ip_address",
+    },
+  ];
+
   return (
     <div className="table-container">
       <h1>Sortable Table with React</h1>
-      <Table />
+      <Table 
+        caption={caption} 
+        data={data} 
+        columns={columns} 
+        />
     </div>
   )
 };
