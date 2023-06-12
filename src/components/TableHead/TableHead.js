1fb14c4866a7d89ac65d2d5e54b1ed32dbd36924
mvaden <mvaden08@gmail.com>
2023-06-12 00:28:30 -0400

    Set sorting change on table headers

--- src/components/TableHead/TableHead.js
+++ src/components/TableHead/TableHead.js
@@ -1,10 +1,45 @@
-const TableHead = ({ columns }) => {
+import { useState } from "react";
+
+const TableHead = ({ columns, handleSorting }) => {
+   const [sortField, setSortField] = useState("");
+   const [order, setOrder] = useState("asc");
+
+   const handleSortingChange = (accessor) => {
+      const sortOrder = accessor === sortField && order === "asc" ? "desc" : "asc";
+      setSortField(accessor);
+      setOrder(sortOrder);
+      handleSorting(accessor, sortOrder);
+   };
+
    return (
       <thead>
          <tr>
             {
-               columns.map(({ label, accessor}) => {
-                  return <th key={accessor} className="table-head">{label}</th>
+               columns.map((
+                  { 
+                     label, 
+                     accessor, 
+                     sortable
+                  }
+               ) => {
+                  const cl = sortable
+                     ? sortField === accessor && order === "asc"
+                        ? "up"
+                        : sortField === accessor && order === "desc"
+                           ? "down"
+                           : "default"
+                     : "";
+                  return (   
+                     <th 
+                        key={accessor} 
+                        onClick={
+                           sortable ? () => handleSortingChange(accessor) : null
+                        }
+                        className={cl}
+                        >
+                        {label}
+                     </th>
+                  )
                })
             }
          </tr>
