ca0030d001e83911ebc44bdcc02bf49b375f4d56
mvaden <mvaden08@gmail.com>
2023-06-12 00:25:11 -0400

    Add sorting prop to columns

--- src/App.js
+++ src/App.js
@@ -4,26 +4,34 @@ import './App.scss';
 const App = () => {
   const caption = "Developers currently enrolled in this course, column headers are sortable.";
   const data = require("./data/data.json");
+  console.log(data)
+
   const columns = [
     {
       label: "First Name",
       accessor: "first_name",
+      sortable: true
     },
     {
       label: "Last Name",
       accessor: "last_name",
+      sortable: false,
+      sortbyOrder: "desc" 
     },
     {
       label: "Email",
       accessor: "email",
+      sortable: true
     },
     {
       label: "Gender",
       accessor: "gender",
+      sortable: true
     },
     {
       label: "IP Address",
       accessor: "ip_address",
+      sortable: true
     },
   ];
 
